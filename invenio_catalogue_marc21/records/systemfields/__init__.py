a316367886e85b6581f98ab70bf28da74cf9787b
Philipp Gualdi <philipp.gualdi@tugraz.at>
2024-09-26 07:07:43 +0000

    feat: create a catalogue record

---
+++ invenio_catalogue_marc21/records/systemfields/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2024 Graz University of Technology.
+#
+# invenio-catalogue-marc21 is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Invenio Marc21 module service record fields modules."""
+
+
+from .catalogue_check import CatalogueCheckField
+
+__all__ = ("CatalogueCheckField",)
