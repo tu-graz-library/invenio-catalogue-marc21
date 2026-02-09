a6c79c520386d53ec95ac372cf9f2f4c943524f9
Christoph Ladurner <christoph.ladurner@tugraz.at>
2026-02-09 12:09:51 +0100

    chore: clean up
    * the docs are not used that way, so for the moment we remove it. if
      they are necessary or helpfull we will add them later. in the meantime
      using the readme file is enough
    
    * the js linter should be used, but also not that important at the
      moment since the files are under heavy development and they are linted
      there directly

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2024 Graz University of Technology.
+# Copyright (C) 2024-2026 Graz University of Technology.
 #
 # invenio-catalogue-marc21 is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
@@ -9,7 +9,7 @@
 
 
 # Usage:
-#   env DB=postgresql12 SEARCH=elasticsearch7 CACHE=redis MQ=rabbitmq ./run-tests.sh
+#  ./run-tests.sh
 
 # Quit on errors
 set -o errexit
@@ -17,17 +17,4 @@ set -o errexit
 # Quit on unbound symbols
 set -o nounset
 
-# Always bring down docker services
-function cleanup() {
-    eval "$(docker-services-cli down --env)"
-}
-trap cleanup EXIT
-
-python -m check_manifest
-python -m sphinx.cmd.build -qnNW docs docs/_build/html
-# TODO: Remove services below that are not neeed (fix also the usage note).
-eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-elasticsearch} --cache ${CACHE:-redis} --mq ${MQ:-rabbitmq} --env)"
 python -m pytest
-tests_exit_code=$?
-python -m sphinx.cmd.build -qnNW -b doctest docs docs/_build/doctest
-exit "$tests_exit_code"
